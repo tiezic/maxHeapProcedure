ccb66ee955e1fa103bcc287432ee683a97b22bf8
isaac <tiezic@gmail.com>
2026-02-15 04:12:51 +0000

    added buildMaxHeap and heapify functions. heapify still needs to be completed as well as the initializing the left and right nodes

--- maxheapmain.java
+++ maxheapmain.java
@@ -1,18 +1,53 @@
 public class maxheapmain {
+    /*
+     * Main method to test buildMaxHeap given an array of WordFreq objects
+     */
     public static void main(String[] args) {
-        WordFreq[] nodesList = {new WordFreq("happy", 400), 
-                                new WordFreq("satisfied", 100),
-                                new WordFreq("neutral", 300),
-                                new WordFreq("would buy again", 200),
-                                new WordFreq("terrible", 160),
-                                new WordFreq("inconvenient", 900),
-                                new WordFreq("difficult to use", 100),
-                                new WordFreq("easy to use", 140),
-                                new WordFreq("would recommend to friends", 800),
-                                new WordFreq("visit the store", 700)};
-        //System.out.println(array[0].word + " " + array[1].frequency);
-        //print certain attributes
+        WordFreq[] array = {new WordFreq("happy", 400), 
+                            new WordFreq("satisfied", 100),
+                            new WordFreq("neutral", 300),
+                            new WordFreq("would buy again", 200),
+                            new WordFreq("terrible", 160),
+                            new WordFreq("inconvenient", 900),
+                            new WordFreq("difficult to use", 100),
+                            new WordFreq("easy to use", 140),
+                            new WordFreq("would recommend to friends", 800),
+                            new WordFreq("visit the store", 700)};
+        buildMaxHeap(array);
+        System.out.println(array.length);
+        System.out.println(11/2);
     }
+
+    /**
+     * Method that creates a max heap using the input array of WordFreq objects.
+     * The method uses heapify to sort the array.
+     */
+    static void buildMaxHeap(WordFreq nodesList[]) {
+        /**
+         * we need the last node that has a leaf, so we divide the length by 2 
+         * then subtract 1. ex: 10 nodes / 2 = 5, 5 - 1 = 4, so 4 is the index
+         * for the last node with a leaf
+         */
+        int lastNodeIndex = (nodesList.length / 2) - 1;
+        
+        /** 
+         * we then decrement to each node, and perform heapify for the parents
+         * and their children.
+         */
+        for(int i = lastNodeIndex; i >= 0; i--) {
+            heapify(nodesList, nodesList.length, i);
+        }
+    }//end buildMaxHeap
+
+    /**
+     * This method takes in the nodesList array, the length of the array, and the
+     * current node we are using as a parent.
+     */
+    static void heapify(WordFreq nodesList[], int n, int i) {
+        int l = i;
+        int r = i;
+        
+    }//end heapify
 
 }
 
@@ -20,8 +55,12 @@ class WordFreq {
     String word;
     int frequency;
 
+    /**
+     * Constructor for WordFreq
+     */
     public WordFreq(String word, int frequency) {
         this.word = word;
         this.frequency = frequency;
     }
-}
+
+}//end WordFreq
