0bc859f3bf5125b5d2d9f17abbf76bc4bfe86191
isaac <tiezic@gmail.com>
2026-02-15 00:58:08 +0000

    added wordfreq class, created array but still needs to be filled in

--- maxheapmain.java
+++ maxheapmain.java
@@ -1,5 +1,17 @@
 public class maxheapmain {
     public static void main(String[] args) {
-        int[] array = {};   
+        WordFreq obj1 = new WordFreq("happy", 400);
+        WordFreq[] array = {obj1};   
+    }
+
+}
+
+class WordFreq {
+    String word;
+    int frequency;
+
+    public WordFreq(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
     }
 }
