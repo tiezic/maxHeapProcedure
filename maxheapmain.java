2352de7d0b5ee1f233141b7d8d7d9e701a731fb6
isaac <tiezic@gmail.com>
2026-02-15 01:35:10 +0000

    added list of nodes/words and their frequencies

--- maxheapmain.java
+++ maxheapmain.java
@@ -1,7 +1,17 @@
 public class maxheapmain {
     public static void main(String[] args) {
-        WordFreq obj1 = new WordFreq("happy", 400);
-        WordFreq[] array = {obj1};   
+        WordFreq[] nodesList = {new WordFreq("happy", 400), 
+                                new WordFreq("satisfied", 100),
+                                new WordFreq("neutral", 300),
+                                new WordFreq("would buy again", 200),
+                                new WordFreq("terrible", 160),
+                                new WordFreq("inconvenient", 900),
+                                new WordFreq("difficult to use", 100),
+                                new WordFreq("easy to use", 140),
+                                new WordFreq("would recommend to friends", 800),
+                                new WordFreq("visit the store", 700)};
+        //System.out.println(array[0].word + " " + array[1].frequency);
+        //print certain attributes
     }
 
 }
